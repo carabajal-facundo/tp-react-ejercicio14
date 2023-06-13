8332ca35f6d1e581436336b69d11d5d07cbc1ac9
carabajal-facundo <facarabajal@hotmail.com>
2023-06-13 00:39:17 -0300

    Se realizo el maquetado del detalle de la receta

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import './App.css'
 import 'bootstrap/dist/css/bootstrap.min.css';
+import Detalle from './assets/components/pages/Detalle';
 
 function App() {
 
   return (
     <>
+    <Detalle></Detalle>
     </>
   )
 }
