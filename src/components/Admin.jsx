865a56e370c33018c19d656c3febb91bbf1c8036
Santiago Antich Llarrull <santiantich98@gmail.com>
2023-06-16 12:22:16 -0300

    Llené información en la tabla

--- src/components/Admin.jsx
+++ src/components/Admin.jsx
@@ -1,41 +1,47 @@
-import { Container } from "react-bootstrap";
-import Table from "react-bootstrap/Table";
+import { Table, Button } from "react-bootstrap";
 const Admin = () => {
   return (
-    <>
-      <Container className="m-5">
+    <div className="container my-5">
         <h1 className="mb-4">Administrador de recetas</h1>
-        <Table striped bordered hover>
+        <Table striped bordered hover responsive>
           <thead>
             <tr>
               <th>#</th>
               <th>Receta</th>
               <th>Ingredientes</th>
-              <th>Username</th>
+              <th>Pasos</th>
+              <th>Imagen</th>
+              <th>Acciones</th>
             </tr>
           </thead>
           <tbody>
             <tr>
               <td>1</td>
-              <td>Mark</td>
-              <td>Otto</td>
-              <td>@mdo</td>
+              <td>Arroz con pollo</td>
+              <td className="text-truncate ancho">Arroz, pollo, queso, leche, aceite, mantequilla</td>
+              <td className="text-truncate ancho">1. Hornear 2.Cocinar 3. Servir</td>
+              <td className="text-truncate ancho">https://upload.wikimedia.org/wikipedia/commons/3/39/Arroz-con-Pollo.jpg</td>
+              <td className="d-flex"><Button>Ver</Button><Button>Borrar</Button></td>
             </tr>
             <tr>
               <td>2</td>
-              <td>Jacob</td>
-              <td>Thornton</td>
-              <td>@fat</td>
+              <td>Empanadas</td>
+              <td className="text-truncate ancho">Thornton</td>
+              <td className="text-truncate ancho">@fat</td>
+              <td className="text-truncate ancho">@fat</td>
+              <td className="text-truncate ancho">@fat</td>
             </tr>
             <tr>
               <td>3</td>
-              <td colSpan={2}>Larry the Bird</td>
-              <td>@twitter</td>
+              <td>Humita</td>
+              <td className="text-truncate ancho">@twitter</td>
+              <td className="text-truncate ancho">@twitter</td>
+              <td className="text-truncate ancho">@twitter</td>
+              <td className="text-truncate ancho">@twitter</td>
             </tr>
           </tbody>
         </Table>
-      </Container>
-    </>
+    </div>
   );
 };
 
