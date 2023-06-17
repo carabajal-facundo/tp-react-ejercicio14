0ac390d9ca12b8fd178378cd3d28e92e2a72ea87
Santiago Antich Llarrull <santiantich98@gmail.com>
2023-06-17 13:17:44 -0300

    Terminé el maquetado de la tabla de administrador

--- src/components/Admin.jsx
+++ src/components/Admin.jsx
@@ -3,7 +3,8 @@ const Admin = () => {
   return (
     <div className="container my-5">
         <h1 className="mb-4">Administrador de recetas</h1>
-        <Table striped bordered hover responsive>
+        <Button>Agregar</Button>
+        <Table striped bordered hover responsive className="my-3">
           <thead>
             <tr>
               <th>#</th>
@@ -21,23 +22,23 @@ const Admin = () => {
               <td className="text-truncate ancho">Arroz, pollo, queso, leche, aceite, mantequilla</td>
               <td className="text-truncate ancho">1. Hornear 2.Cocinar 3. Servir</td>
               <td className="text-truncate ancho">https://upload.wikimedia.org/wikipedia/commons/3/39/Arroz-con-Pollo.jpg</td>
-              <td className="d-flex"><Button>Ver</Button><Button>Borrar</Button></td>
+              <td className="d-flex"><Button variant="warning">Editar</Button><Button variant="danger">Borrar</Button></td>
             </tr>
             <tr>
               <td>2</td>
               <td>Empanadas</td>
-              <td className="text-truncate ancho">Thornton</td>
-              <td className="text-truncate ancho">@fat</td>
-              <td className="text-truncate ancho">@fat</td>
-              <td className="text-truncate ancho">@fat</td>
+              <td className="text-truncate ancho">Carne picada, picante, tapas para empanada</td>
+              <td className="text-truncate ancho">1. Cortar la carne 2. Cocinar 3. Comer</td>
+              <td className="text-truncate ancho">https://assets.bonappetit.com/photos/58a34e1df12ac6e639bf24ae/1:1/w_2812,h_2812,c_limit/argentinian-beef-empanadas.jpg</td>
+              <td className="d-flex"><Button variant="warning">Editar</Button><Button variant="danger">Borrar</Button></td>
             </tr>
             <tr>
               <td>3</td>
               <td>Humita</td>
-              <td className="text-truncate ancho">@twitter</td>
-              <td className="text-truncate ancho">@twitter</td>
-              <td className="text-truncate ancho">@twitter</td>
-              <td className="text-truncate ancho">@twitter</td>
+              <td className="text-truncate ancho">Choclo rallado, queso, aji</td>
+              <td className="text-truncate ancho">1. Rallar el choclo 2. Hervir 3. Servir caliente</td>
+              <td className="text-truncate ancho">https://canalcocina.es/medias/images/1403SorLuciaHumita.jpg</td>
+              <td className="d-flex"><Button variant="warning">Editar</Button><Button variant="danger">Borrar</Button></td>
             </tr>
           </tbody>
         </Table>
