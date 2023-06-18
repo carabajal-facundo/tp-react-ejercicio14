a6b507e5ae205801fd7a88d4cf0cb944b519918d
Santiago Antich Llarrull <santiantich98@gmail.com>
2023-06-17 21:58:39 -0300

    Terminé el crearReceta

--- src/components/Admin.jsx
+++ src/components/Admin.jsx
@@ -1,45 +1,96 @@
 import { Table, Button, Modal, Form } from "react-bootstrap";
 import { useState } from "react";
+
 const Admin = () => {
   const [show, setShow] = useState(false);
+  const [receta, setReceta] = useState({
+    nombre: "",
+    ingredientes: "",
+    pasos: "",
+    imagen: ""
+  });
+  let recetasLocalStorage = JSON.parse(localStorage.getItem('recetas')) || [];
+  const [recetas, setRecetas] = useState(recetasLocalStorage);
+
   const handleClose = () => setShow(false);
   const handleShow = () => setShow(true);
 
+  const handleInputChange = (e) => {
+    const { id, value } = e.target;
+    setReceta((prevReceta) => ({
+      ...prevReceta,
+      [id]: value
+    }));
+  };
+  const borrarReceta = (id) => {
+    const newRecetas = recetas.filter((receta) => receta.id !== id);
+    setRecetas(newRecetas);
+    localStorage.setItem('recetas', JSON.stringify(newRecetas));
+  }
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setRecetas([...recetas, receta]);
+    localStorage.setItem("recetas", JSON.stringify(recetas));
+    handleClose();
+  };
+
   return (
     <div className="container my-5">
-        <h1 className="mb-3">Administrador de recetas</h1>
-        <Button variant="primary" onClick={handleShow}>
+      <h1 className="mb-3">Administrador de recetas</h1>
+      <Button variant="primary" onClick={handleShow}>
         Agregar
-        </Button>
-        <Modal show={show} onHide={handleClose}>
+      </Button>
+      <Modal show={show} onHide={handleClose}>
         <Modal.Header closeButton>
           <Modal.Title>Agregar o modificar receta</Modal.Title>
         </Modal.Header>
         <Modal.Body>
-          <Form>
-          <Form.Group className="mb-3" controlId="formReceta">
-        <Form.Label>Receta</Form.Label>
-        <Form.Control type="text" placeholder="Nombre del plato" />
-      </Form.Group>
-      <Form.Group className="mb-3" controlId="formIngredientes">
-        <Form.Label>Ingredientes</Form.Label>
-        <Form.Control as="textarea" rows={3} type="text" placeholder="Ej: Arroz, 2 huevos, manteca" />
-      </Form.Group>
-      <Form.Group className="mb-3" controlId="formPasos">
-        <Form.Label>Pasos</Form.Label>
-        <Form.Control as="textarea" rows={3} type="text" placeholder="Ej: 1. Hornear 2. Servir 3. Limpiar" />
-      </Form.Group>
-      <Form.Group className="mb-3" controlId="formImagen">
-        <Form.Label>Imagen</Form.Label>
-        <Form.Control type="text" placeholder="URL de la imagen" />
-      </Form.Group>
+          <Form onSubmit={handleSubmit}>
+            <Form.Group className="mb-3" controlId="nombre">
+              <Form.Label>Receta</Form.Label>
+              <Form.Control
+                type="text"
+                placeholder="Nombre del plato"
+                value={receta.nombre}
+                onChange={handleInputChange}
+              />
+            </Form.Group>
+            <Form.Group className="mb-3" controlId="ingredientes">
+              <Form.Label>Ingredientes</Form.Label>
+              <Form.Control
+                as="textarea"
+                rows={3}
+                type="text"
+                placeholder="Ej: Arroz, 2 huevos, manteca"
+                value={receta.ingredientes}
+                onChange={handleInputChange}
+              />
+            </Form.Group>
+            <Form.Group className="mb-3" controlId="pasos">
+              <Form.Label>Pasos</Form.Label>
+              <Form.Control
+                as="textarea"
+                rows={3}
+                type="text"
+                placeholder="Ej: 1. Hornear 2. Servir 3. Limpiar"
+                value={receta.pasos}
+                onChange={handleInputChange}
+              />
+            </Form.Group>
+            <Form.Group className="mb-3" controlId="imagen">
+              <Form.Label>Imagen</Form.Label>
+              <Form.Control
+                type="text"
+                placeholder="URL de la imagen"
+                value={receta.imagen}
+                onChange={handleInputChange}
+              />
+            </Form.Group>
+            <Button variant="primary" type="submit">
+              Guardar receta
+            </Button>
           </Form>
         </Modal.Body>
-        <Modal.Footer>
-          <Button variant="primary" type="submit" onClick={handleClose}>
-            Guardar receta
-          </Button>
-        </Modal.Footer>
       </Modal>        
         <Table striped bordered hover responsive className="my-3">
           <thead>
@@ -59,7 +110,7 @@ const Admin = () => {
               <td className="text-truncate ancho">Arroz, pollo, queso, leche, aceite, mantequilla</td>
               <td className="text-truncate ancho">1. Hornear 2.Cocinar 3. Servir</td>
               <td className="text-truncate ancho">https://upload.wikimedia.org/wikipedia/commons/3/39/Arroz-con-Pollo.jpg</td>
-              <td className="d-flex justify-content-evenly"><Button variant="warning" onClick={handleShow}>Editar</Button><Button variant="danger">Borrar</Button></td>
+              <td className="d-flex justify-content-evenly"><Button variant="warning" onClick={handleShow}>Editar</Button><Button variant="danger" >Borrar</Button></td>
             </tr>
             <tr>
               <td>2</td>
