d93e060000bae2254b81983102d5886346324f09
facundo cajal <facundocajal199@gmail.com>
2023-06-21 14:12:48 -0300

    index completado

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import Detalle from './assets/components/pages/Detalle';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import NavBar from './components/NavBar';
-
+import Index from './components/Index';
 function App() {
 
   return (
@@ -13,7 +13,8 @@ function App() {
         <Routes>
           <Route exact path="/administrador" element={<Admin></Admin>}></Route>
           <Route exact path="/detalle" element={<Detalle></Detalle>}></Route>
-          <Route path="*" element={<Admin></Admin>}></Route>
+          <Route exact path="/" element={<Index></Index>}></Route>
+          <Route path="*" element={<Index></Index>}></Route>
         </Routes>
     </BrowserRouter>
 
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -6,7 +6,7 @@ const NavBar = () => {
     return (
         <Navbar bg="dark" data-bs-theme="dark">
         <Container>
-          <Navbar.Brand as={Link} to='/administrador'>Blog-Cocina</Navbar.Brand>
+          <Navbar.Brand as={Link} to='/Index'>Blog-Cocina</Navbar.Brand>
           <Nav className="me-auto">
             <NavLink end to={'/administrador'} className='nav-item nav-link'>Arministrador</NavLink>
             <NavLink end to={'/detalle'} className='nav-item nav-link'>Detalle</NavLink>
