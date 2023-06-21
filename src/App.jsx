20e2ac9303238376444ef51b4894dea7f508d38a
carabajal-facundo <facarabajal@hotmail.com>
2023-06-21 16:12:13 -0300

    Pequeña correccion

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
 import Admin from './components/Admin';
-import Detalle from './components/Detalle'
 import './App.css'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
